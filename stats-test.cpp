8b6c20dceea205167595c0d4f5397f8d4e40d113
Harshitha0306 <77484111+Harshitha0306@users.noreply.github.com>
2021-01-26 03:59:16 +0530

    Update stats-test.cpp

--- stats-test.cpp
+++ stats-test.cpp
@@ -1,4 +1,4 @@
-define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 //#include <iostream>
 #include "catch.hpp"
 #include "decl.hpp"
